cb367deb2a8a7cff232bc7bb4b8d40afd9f7623d
Matthew <78713317+mreed4@users.noreply.github.com>
2023-09-25 21:33:02 -0700

    Palette change

--- src/Components/Pages/StartPage.jsx
+++ src/Components/Pages/StartPage.jsx
@@ -1,48 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useRef } from "react";
+import { TextLoop } from "easy-react-text-loop";
 
 import "../../assets/css/StartPage.css";
 
-// function cycleThroughWords() {
-//   const words = ["build", "design", "develop", "create", "grow"];
-
-//   let i = 0;
-
-//   setInterval(() => {
-//     const word = document.getElementById("word");
-//     word.innerText = words[i];
-//     i = (i + 1) % words.length;
-//   }, 1000);
-// }
-
 export default function StartPage() {
-  const location = useLocation();
-
-  const wordRef = useRef();
-
-  function cycleThroughWords() {
-    const words = ["build", "design", "develop", "create", "grow"];
-
-    let i = 0;
-
-    setInterval(() => {
-      wordRef.current.innerText = words[i];
-      i = (i + 1) % words.length;
-    }, 1000);
-  }
-
-  useEffect(() => {
-    if (location.pathname === "/") {
-      cycleThroughWords();
-    }
-  }, []);
-
   return (
     <div id="start-page" className="app-page fade-in">
       <section id="intro">
         <div>
           <p>
-            We connect you to others to help you <span id="word" className="word" ref={wordRef}></span>.
+            We connect you to others to help you&nbsp;
+            <TextLoop animation="tween">
+              <span className="word">create.</span>
+              <span className="word">develop.</span>
+              <span className="word">build.</span>
+              <span className="word">design.</span>
+            </TextLoop>
           </p>
         </div>
       </section>
@@ -50,8 +24,8 @@ export default function StartPage() {
         <div>
           <h2>About</h2>
           <p>
-            Pairings is firstly a platform that matches new designers with new developers. If you need to pad your portfolio, or just want
-            to work on a project with someone else, then this is the place for you.
+            Pairings is a platform that <span className="highlight">pairs new designers with new developers</span>. If you need to pad your
+            portfolio, or just want to work on a project with someone else, then this is the place for you.
           </p>
           <p>It can also be used to pair new developers with more experienced developers.</p>
           <p>We believe that the best way to learn is to work with others, and we want to help you find the right people to work with.</p>
@@ -60,32 +34,32 @@ export default function StartPage() {
       <section id="features">
         <div>
           <h2>Features</h2>
-          <div className="features-feature-list">
-            <div className="features-feature">
+          <div className="features-list">
+            <div className="feature">
               <h3>
                 <span className="material-symbols-outlined">view_kanban</span>Kanban Board
               </h3>
               <p>Create a kanban board to keep track of your project.</p>
             </div>
-            <div className="features-feature">
+            <div className="feature">
               <h3>
                 <span className="material-symbols-outlined">speaker_notes</span>Chat
               </h3>
               <p>Create a chat for your project. Talk about your project, and ask questions.</p>
             </div>
-            <div className="features-feature">
+            <div className="feature">
               <h3>
                 <span className="material-symbols-outlined">calendar_month</span>Project calendar
               </h3>
               <p>Create a calendar for your project. Keep track of important dates, and deadlines.</p>
             </div>
-            <div className="features-feature">
+            <div className="feature">
               <h3>
                 <span className="material-symbols-outlined">stars</span>Project generator
               </h3>
               <p>Generate a project for you to work on with someone else.</p>
             </div>
-            <div className="features-feature">
+            <div className="feature">
               <h3>
                 <span className="material-symbols-outlined">code_blocks</span> Code review
               </h3>
