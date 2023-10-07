8a0533b262f999a61b7ba060f85b298fceb8e7b7
Matthew <78713317+mreed4@users.noreply.github.com>
2023-10-07 14:53:18 -0700

    Tweaks

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,23 +5,38 @@ import "../assets/css/Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-logo">
-        <Link to="/">
-          <span className="logo">Pairings</span>
-        </Link>
+      <div className="footer-top">
+        <div className="footer-logo">
+          <Link to="/">
+            <span className="logo">pairings</span>
+          </Link>
+        </div>
+        <div className="footer-links">
+          <ol>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sign-in">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ol>
+        </div>
       </div>
-      <div className="footer-links">
-        <ol>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sign-in">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ol>
+      <div className="footer-bottom italic">
+        <p>
+          &copy; {new Date().getFullYear()} pairings. All rights reserved. Hero image by{" "}
+          <a href="https://www.pexels.com/@fauxels/" target="_blank" rel="noreferrer">
+            @fauxels
+          </a>{" "}
+          from{" "}
+          <a href="https://www.pexels.com/" target="_blank" rel="noreferrer">
+            Pexels
+          </a>
+          .
+        </p>
       </div>
     </footer>
   );
